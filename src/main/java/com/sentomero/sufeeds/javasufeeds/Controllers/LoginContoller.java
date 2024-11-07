297a990640c81fb38f154cb076787ab47f9ad326
Jeremiah <sentomerojeremy@gmail.com>
2024-11-07 21:35:11 +0300

    new work

--- src/main/java/com/sentomero/sufeeds/javasufeeds/Controllers/LoginContoller.java
+++ src/main/java/com/sentomero/sufeeds/javasufeeds/Controllers/LoginContoller.java
@@ -1,14 +1,15 @@
-package com.sentomero.sufeeds.javasufeeds;
+package com.sentomero.sufeeds.javasufeeds.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginContoller {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onHelloButtonClick()
+    {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,6 @@ module com.sentomero.sufeeds.javasufeeds {
 
     opens com.sentomero.sufeeds.javasufeeds to javafx.fxml;
     exports com.sentomero.sufeeds.javasufeeds;
+    exports com.sentomero.sufeeds.javasufeeds.Controllers;
+    opens com.sentomero.sufeeds.javasufeeds.Controllers to javafx.fxml;
 }
